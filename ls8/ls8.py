93eb4c842fea683ed8b8d63ed61c84b4514ea08d
Trevor Martin <trevor.j.martin@gmail.com>
2020-06-16 14:17:08 -0400

    load program from file

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -3,9 +3,29 @@
 """Main."""
 
 import sys
-from cpu import *
+from cpu import CPU
 
-cpu = CPU()
 
-cpu.load()
-cpu.run()
+def main(program_file):
+    if program_file is None:
+        program_file = "ls8/examples/print8.ls8"
+    with open(program_file) as f:
+        program = list(map(lambda n: int(n, 2), list(filter(lambda s: len(s) > 0, list(
+            map(lambda code: code.split('#')[0].strip(), f.readlines()))))))
+    cpu = CPU()
+    cpu.load(program)
+    cpu.run()
+
+
+if __name__ == "__main__":
+    try:
+        source = sys.argv[0]
+        program = sys.argv[1]
+    except IndexError as ie:
+        print("\n    ---------------------- ")
+        print("      REQUIRES A PROGRAM ")
+        print("    ---------------------- \n")
+        print("***   USAGE: python3 ls8.py PROGRAM   ***")
+        print('\n*** EXAMPLE: python3 ls8.py examples/print8.ls8 ***\n')
+        exit(0)
+    main(program)
